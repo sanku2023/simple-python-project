18edb61425fe49d30f253efc0eacdcef435a34a7
Sneha <shaharesp@gmail.com>
2024-12-03 14:11:27 +0530

    your details

---
+++ detail.py
@@ -0,0 +1,9 @@
+#detail.py
+
+class Address:
+    def __init_(self, name, street, city, state):
+        self.name = name
+        self.street = street
+        self.city = city
+        self.state = state
+     
